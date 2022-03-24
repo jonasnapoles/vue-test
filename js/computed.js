434d966a9976d250857444760aa7258aa215541a
jonasnapoles <jonasnapoles@gitgub.com>
2022-03-24 23:39:30 +0000

    update files Fifecycle example

--- js/computed.js
+++ js/computed.js
@@ -1,12 +1,21 @@
 const app = new Vue({
-    el: "#app",
-    data: {
-        message: 'Hello Mate',
-        accountant: 0
+  el: "#app",
+  data: {
+    message: "Hello Mate",
+    accountant: 0,
+  },
+  // Computed Properties
+  computed: {
+    inverted() {
+      return this.message.split("").reverse().join("");
     },
-    computed:{
-        inverted(){
-            return this.message.split('').reverse().join('');
-        }
-    }
-})
+    colour() {
+      // this is returning an object
+      return {
+        "bg-success": this.accountant <= 30,
+        "bg-warning": this.accountant > 30 && this.accountant < 60,
+        "bg-danger": this.accountant >= 60,
+      };
+    },
+  },
+});
---
+++ js/lifecycle.js
@@ -0,0 +1,4 @@
+const app = new VTTCue({
+  el: "#app",
+  data: {},
+});
