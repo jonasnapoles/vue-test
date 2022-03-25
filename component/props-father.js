7a0ad2a25ed350dffccc72994dff6c074c3da662
jonasnapoles <jonasnapoles@gitgub.com>
2022-03-25 15:48:20 +0000

    update files about components with examples

---
+++ component/props-father.js
@@ -0,0 +1,10 @@
+Vue.component("father", {
+
+  template:   //html 
+    `
+    <div class="p-5 bg-dark text-white">
+    <h2>Father component</h2>
+    <son>Son component</son>
+    </div>
+    `,
+});
---
+++ component/props-son.js
@@ -0,0 +1,8 @@
+Vue.component('son', {
+    template: //html
+    `
+    <div class="py-5 bg-success">
+        <h4>Son component</h4>
+    </div>
+    `
+});
