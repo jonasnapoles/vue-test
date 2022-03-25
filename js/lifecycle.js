e883851f0a0ff85efbf2cc3ae49d32be03e8ca9b
jonasnapoles <jonasnapoles@gitgub.com>
2022-03-25 01:55:18 +0000

    Update files lifecycle with finished examples

--- js/lifecycle.js
+++ js/lifecycle.js
@@ -1,4 +1,43 @@
-const app = new VTTCue({
+const app = new Vue({
   el: "#app",
-  data: {},
+  data: {
+    greeting: "I'm lifecycle of Vue",
+  },
+  beforeCreate() {
+    console.log("beforeCreate");
+  },
+  created() {
+    //When creating the methods, observers and events, but it still doesn't access the 'DOM'.
+    //Still can´t acess 'el'
+    console.log("created");
+  },
+  beforeMount() {
+    //It's runs before inserting the 'DOM'
+    console.log("beforeMount");
+  },
+  mounted() {
+    //It's runs to the 'DOM' insert
+    console.log("mounted");
+  },
+  beforeUpdate() {
+    //It's runs when detecting a change
+    console.log("beforeUpdate");
+  },
+  updated() {
+    //It's runs wheren making the changes
+    console.log("updated");
+  },
+  beforeDestroy() {
+    //It's executed before ddestroying the instance
+    console.log("beforeDestroy");
+  },
+  destroyed() {
+    //When executing It's destroyed the entire instance
+    console.log("destroyed");
+  },
+  methods: {
+    destroy() {
+      this.$destroy();
+    },
+  },
 });
