459ed29931a3df6529620eb6f6c6ba37507cbad5
jonasnapoles <jonasnapoles@gitgub.com>
2022-03-25 16:21:42 +0000

    Props examples

--- component/props-father.js
+++ component/props-father.js
@@ -3,8 +3,17 @@ Vue.component("father", {
   template:   //html 
     `
     <div class="p-5 bg-dark text-white">
-    <h2>Father component</h2>
-    <son>Son component</son>
+    <h2>Father component: {{fatherNumber}}</h2>
+    <button class="btn btn-danger" @click="fatherNumber++">+</button>
+    <son :number="fatherNumber">Son component</son>
+    
     </div>
     `,
+    data(){
+        return{
+            fatherNumber: 0
+        }
+    }
+
+
 });
--- component/props-son.js
+++ component/props-son.js
@@ -2,7 +2,11 @@ Vue.component('son', {
     template: //html
     `
     <div class="py-5 bg-success">
-        <h4>Son component</h4>
+
+        <h4 >Son component</h4>
+        <h5>{{number}}</h5>
     </div>
-    `
+    `,
+        // Props allow communication between components (father and son)
+        props: ['number']
 });
